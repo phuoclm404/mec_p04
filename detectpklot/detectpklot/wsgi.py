a68d37e40b6660f7eb156989cef0971ad19cb1ae
phuoclm <leminhphuoc04@gmail.com>
2023-07-13 17:10:07 +0700

    13/7

--- detectpklot/detectpklot/wsgi.py
+++ detectpklot/detectpklot/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'detectpklot.settings')
 
 application = get_wsgi_application()
+from detectpklot.wsgi import application
